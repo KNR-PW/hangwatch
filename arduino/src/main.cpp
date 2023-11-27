6faf699f8191c71a1f4323ca19a325f52b696521
OlgierdIwaniuk <oloiwani@gmail.com>
2023-11-27 14:37:24 +0100

    Update main.cpp
    dodaliśmy do modelu 2 diody jedna włącza się automatycznie jeśli klucze wiszą na haczyku a druga jeżeli informacja ta została wysłana na stronę

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -2,13 +2,19 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-
+#define LED 2
+#define LED2 4
 
 const char* ssid = "Telefon MI";
 const char* password = "Barcelona1";
 
 const char* serverAddress = "http://192.168.43.212:5000/gpio_state";
+<<<<<<< Updated upstream
 
+=======
+bool secondledstate=false;
+bool automaticLedState = false;
+>>>>>>> Stashed changes
 void setup(){
     Serial.begin(115200);
     delay(1000);
@@ -21,11 +27,14 @@ void setup(){
         Serial.print(".");
         delay(100);
     }
-
-    
+    pinMode(LED,OUTPUT);
+    pinMode(LED2,OUTPUT);
 }
 
 void loop() {
+     if (!automaticLedState) {
+        digitalWrite(LED, HIGH);
+    }
     // Odczytaj stan GPIO
    int gpioState = 10 ;
     DynamicJsonDocument jsonDoc(200);
@@ -36,7 +45,13 @@ void loop() {
     HTTPClient http;
     http.begin(serverAddress);
     http.addHeader("Content-Type", "application/json");
+<<<<<<< Updated upstream
      int httpResponseCode = http.sendRequest("POst", payload);
+=======
+     int httpResponseCode = http.sendRequest("POST", payload);
+    if  (!secondledstate) 
+    { digitalWrite(LED2, HIGH); }
+>>>>>>> Stashed changes
     if(httpResponseCode > 0)
     {
         Serial.printf("HTTP Response code: %d\n",httpResponseCode );
@@ -44,16 +59,25 @@ void loop() {
         DynamicJsonDocument jsonDoc(200);
         deserializeJson(jsonDoc, response);
         String success = jsonDoc["success"].as<String>();
+         if (success == "true") { secondledstate = true; digitalWrite(LED2, HIGH); }
         Serial.println(response);
-
+        
+        digitalWrite(LED,HIGH);
+        delay(1000);
+        digitalWrite(LED,LOW);
     }
     else
     {
         Serial.printf("HTTP Request failed %s\n", http.errorToString(httpResponseCode).c_str());
-
+        digitalWrite(LED2, LOW);
     }
-  //Serial.print(code);
-    // Poczekaj przez określony czas
+     if (automaticLedState) {
+        digitalWrite(LED, LOW);
+        automaticLedState = false;
+    }
+  
     http.end();
+    
     delay(1000);
+   
 }
