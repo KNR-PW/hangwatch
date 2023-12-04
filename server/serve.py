f276d504610df8d2bf7cd1d85ff7731d4192ea88
OlgierdIwaniuk <oloiwani@gmail.com>
2023-12-04 15:03:04 +0100

    Update serve.py

--- server/serve.py
+++ server/serve.py
@@ -8,35 +8,44 @@ lastUpdate = time.time()
 
 state = {}
 
-@app.route('/hooks', methods=['POST','GET'])
+@app.route('/hooks', methods=['POST'])
 def receive_data():
-    global lastUpdate
+    
     try:
 
         content = request.get_json() 
         if 'state' in content:
          received_state = content['state']
-         state['miejsce'] = {} 
-         state['miejsce']['state'] = received_state
-         state['miejsce']=content['miejsce']
+         state['place'] = {} 
+         state['place']['state'] = received_state
+         state['place']=content['place']
          state['state']=content['state']
-         lastUpdate = time.time() # Zaktualizuj czas ostatniej aktualizacji
-        
-         print(f"Miejsce: {state['miejsce']}, Stan: {state['state']}")
-        
-# Sprawdź, czy czas od ostatniej aktualizacji przekroczył 2 godziny
-        if time.time() - lastUpdate > 7200:
-            state['state'] = 'offline'
+          # Zaktualizuj czas ostatniej aktualizacji
+        # Zaktualizuj czas ostatniej aktualizacji
+      
+         
+         print(f"place: {state['place']}, State: {state['state']}")
+    
         return jsonify({'success': True})
     except Exception as e:
         print(f"Error receiving state: {str(e)}")
         return jsonify({'success': False, 'error': str(e)})
 
 
-if time.time() - lastUpdate > 10:
-    state['state'] = 'empty'
+@app.route('/hooks', methods=['GET'])
+def check_state():
+    
+    if time.time() - lastUpdate > 10:
+        lastUpdate = time.time()
+        state['state'] = 'empty' 
+    elif time.time() - lastUpdate > 7200:
+        state['state'] = 'offline'
+    return jsonify(state)
 
 
+@app.route('/state', methods=['GET'])
+def get_state():
+    return jsonify(state)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
