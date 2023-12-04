af9d3074f3ed6f8f5ec36db89d6b81dec7f4688d
OlgierdIwaniuk <oloiwani@gmail.com>
2023-12-04 18:55:52 +0100

    Update serve.py

--- server/serve.py
+++ server/serve.py
@@ -3,14 +3,14 @@ from flask import Flask, request,  jsonify
 
 app = Flask(__name__, template_folder='template')
 
-lastUpdate = time.time()
+
 
 
 state = {}
 
 @app.route('/hooks', methods=['POST'])
 def receive_data():
-    
+    lastUpdate = time.time()
     try:
 
         content = request.get_json() 
@@ -20,10 +20,8 @@ def receive_data():
          state['place']['state'] = received_state
          state['place']=content['place']
          state['state']=content['state']
-          # Zaktualizuj czas ostatniej aktualizacji
-        # Zaktualizuj czas ostatniej aktualizacji
-      
-         
+         state['time']=lastUpdate
+        
          print(f"place: {state['place']}, State: {state['state']}")
     
         return jsonify({'success': True})
@@ -34,18 +32,14 @@ def receive_data():
 
 @app.route('/hooks', methods=['GET'])
 def check_state():
+    if (time.time() - state['time']) > 7220:
+         state['state'] = 'offline'
+    elif (time.time() - state['time']) > 10:
+        state['state'] = 'empty'    
     
-    if time.time() - lastUpdate > 10:
-        lastUpdate = time.time()
-        state['state'] = 'empty' 
-    elif time.time() - lastUpdate > 7200:
-        state['state'] = 'offline'
+    
     return jsonify(state)
 
-
-@app.route('/state', methods=['GET'])
-def get_state():
-    return jsonify(state)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
