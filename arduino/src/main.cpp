dcd1deb57e892b06ae0d91aa85c478077154dc1c
OlgierdIwaniuk <oloiwani@gmail.com>
2023-12-08 20:21:42 +0100

    Update main.cpp

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -4,10 +4,14 @@
 #include <ArduinoJson.h>
 
 
+
 const char* ssid = "Telefon MI";
 const char* password = "Barcelona1";
 
-const char* serverAddress = "http://192.168.43.212:5000/get_gpio_state";
+const char* serverAddress = "http://192.168.43.212:5000/hooks";
+unsigned long previousTime = 0; // Zmienna przechowująca poprzednią wartość millis()
+const unsigned long interval = 60 * 5*1000; // Czas w milisekundach (5 minut)
+const char* board_id = "1234";
 
 void setup(){
     Serial.begin(115200);
@@ -20,40 +24,87 @@ void setup(){
     while(WiFi.status() != WL_CONNECTED){
         Serial.print(".");
         delay(100);
+        
     }
-
-    
+    pinMode(33,OUTPUT);
+    pinMode(25,INPUT_PULLUP);
 }
-
 void loop() {
-    // Odczytaj stan GPIO
-   int gpioState = 10 ;
-    DynamicJsonDocument jsonDoc(200);
-    jsonDoc["gpioState"] = gpioState;
-    String payload;
-    serializeJson(jsonDoc,payload);
-    // Wyślij żądanie POST na serwer Flask
-    HTTPClient http;
-    http.begin(serverAddress);
-    http.addHeader("Content-Type", "application/json");
-     int httpResponseCode = http.sendRequest("GET", payload);
-    if(httpResponseCode > 0)
+    
+    
+    int przyciskState = digitalRead(25);
+   
+    if(przyciskState==LOW)
     {
-        Serial.printf("HTTP Response code: %d\n",httpResponseCode );
+        digitalWrite(33,HIGH);
+        // Odczytaj stan GPIO
+        const char* miejsce="warsztat 027";
+        DynamicJsonDocument jsonDoc(200);
+        jsonDoc["place"] =miejsce ;
+        jsonDoc["state"]="hanged";
+        jsonDoc["board_id"] = board_id;
+        String payload;
+        serializeJson(jsonDoc,payload);
+        // Wyślij żądanie POST na serwer Flask
+        HTTPClient http;
+        http.begin(serverAddress);
+        http.addHeader("Content-Type", "application/json");
+        int httpResponseCode = http.sendRequest("POST", payload);
+        
+        if(httpResponseCode > 0)
+        {
+           
+            Serial.printf("HTTP Response code: %d\n",httpResponseCode );
+            String response = http.getString();
+            Serial.print(miejsce);
+            DynamicJsonDocument jsonDoc(200);
+            deserializeJson(jsonDoc, response);
+            String success = jsonDoc["success"].as<String>();
+            Serial.println(response);
+        }
+        else
+        { 
+            Serial.printf("HTTP Request failed %s\n", http.errorToString(httpResponseCode).c_str()); 
+        }
+        http.end();
+    }
+    else
+   {
+        digitalWrite(33,LOW);
+        delay(1000);
+    }
+    
+    if (millis() - previousTime >= interval) 
+    {
+        previousTime = millis();
+
+        // Wysyłaj informacje co 5 minut niezależnie od stanu przycisku
+        const char* miejsce = "warsztat 027";
+        DynamicJsonDocument jsonDoc(200);
+        jsonDoc["place"] = miejsce;
+        jsonDoc["state"] = "check";
+        jsonDoc["board_id"] = board_id;
+        String payload;
+        serializeJson(jsonDoc, payload);
+        // Wyślij żądanie POST na serwer Flask
+        HTTPClient http;
+        http.begin(serverAddress);
+        http.addHeader("Content-Type", "application/json");
+        int httpResponseCode = http.sendRequest("POST", payload);
+        delay(1000);
+    if (httpResponseCode > 0) {
+        Serial.printf("HTTP Response code: %d", httpResponseCode);
         String response = http.getString();
         DynamicJsonDocument jsonDoc(200);
         deserializeJson(jsonDoc, response);
         String success = jsonDoc["success"].as<String>();
         Serial.println(response);
-
     }
-    else
-    {
-        Serial.printf("HTTP Request failed %s\n", http.errorToString(httpResponseCode).c_str());
-
+    else {
+        Serial.printf("HTTP Request failed %s", http.errorToString(httpResponseCode).c_str());
     }
-  //Serial.print(code);
-    // Poczekaj przez określony czas
     http.end();
-    delay(1000);
+    }
+    
 }
+
